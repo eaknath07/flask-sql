fea710354df82a2fc6372ba48e2f5adb1462291a
AjithaRQ <ajitha.devi@rootquotient.com>
2025-10-27 18:29:09 +0530

    added health and procfile

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask is running successfully!"
 
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
 if __name__ == "__main__":
     app.run(debug=True)
